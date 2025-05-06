67ae050968006e2fef794962e8600ec4789604f8
Geza1985 <atomgeza@freemail.hu>
2025-05-06 23:57:50 +0200

    Ötödik részlet [Flámis Géza-NGEK9N]

---
+++ pages/home.php
@@ -0,0 +1,17 @@
+<h2>Üdvözlünk a PC Parts Shop oldalán!</h2>
+<p>Nálunk megtalálod a legjobb alkatrészeket: processzorokat, videokártyákat, memóriákat és sok mást!</p>
+
+<h3>Bemutatkozó videó</h3>
+<video width="320" height="240" controls>
+  <source src="uploads/intro.mp4" type="video/mp4">
+  A böngésződ nem támogatja a videólejátszást.
+</video>
+
+<h3>Ismertető videó (YouTube-ról)</h3>
+<iframe width="560" height="315" src="https://www.youtube.com"
+        title="YouTube videó" frameborder="0" allowfullscreen></iframe>
+
+<h3>Üzletünk helye a térképen</h3>
+<iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10787.818824267582!2d19.0688376!3d47.5271849!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4741dc1c4e250cf7%3A0x6a3c04e56aa2c06e!2zQnVkYXBlc3QsIFbDoWNpIMOzaXQxLCDDqXphcnN6w6Fnbw!5e0!3m2!1shu!2shu!4v1714837000000"
+    width="600" height="450" style="border:0;" allowfullscreen loading="lazy"></iframe>
